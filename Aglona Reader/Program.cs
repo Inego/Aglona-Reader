fc324ac80881a4c99c88f172eee2b478920095fe
Yanis Batura <yanis.batura@gmail.com>
2020-05-16 10:35:30 +0700

    Fix remaining ReSharper issues

--- Aglona Reader/Program.cs
+++ Aglona Reader/Program.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿﻿using System;
 using System.Windows.Forms;
 
 namespace AglonaReader
 {
+    // ReSharper disable once UnusedType.Global
     internal static class Program
     {
         [STAThread]
+        // ReSharper disable once UnusedMember.Local
         private static void Main()
         {
             Application.EnableVisualStyles();
